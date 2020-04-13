6020f6f5ec792187ce058accecc17435823bd934
ta <amida.tatsuya@gmail.com>
2020-04-13 22:48:29 +0900

    プロジェクトを作成

---
+++ DiscomfortIndexApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiscomfortIndexApp
+//
+//  Created by Tatsuya Amida on 2020/04/13.
+//  Copyright © 2020 T.A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
