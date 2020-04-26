988b7b929d4aa2de7309250b53a845ddf254ea16
ta <amida.tatsuya@gmail.com>
2020-04-26 14:30:17 +0900

    feat-天気の名称を取得できるようにしました

--- DiscomfortIndexApp/Model/WeatherManager.swift
+++ DiscomfortIndexApp/Model/WeatherManager.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=[apiKey]&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=65d6c9feda17974714fa85250b53a1ec&units=metric"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
@@ -36,12 +36,36 @@ struct WeatherManager {
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
             print(decodedData.name)
             print(decodedData.weather[0].description)
             print(decodedData.main.temp)
             print(decodedData.main.humidity)
+            print(getConditionName(weatherId: id))
         } catch {
             print(error)
         }
     }
+    
+    func getConditionName(weatherId: Int) -> String {
+        switch weatherId {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...321:
+            return "cloud.drizzle"
+        case 500...531:
+            return "cloud.rain"
+        case 600...622:
+            return "cloud.snow"
+        case 701...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        case 801...804:
+            return "cloud.bolt"
+        default:
+            return "cloud"
+        }
+    }
+    
 }
