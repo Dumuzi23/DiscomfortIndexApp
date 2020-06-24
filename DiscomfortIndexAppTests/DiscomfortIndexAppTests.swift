99538e3c2796cf9e2db63d0d83df1cd27d32da45
ta <amida.tatsuya@gmail.com>
2020-06-24 21:03:57 +0900

    fix-WeatherModelTestsのテスト構成を修正しました

--- DiscomfortIndexAppTests/DiscomfortIndexAppTests.swift
+++ DiscomfortIndexAppTests/DiscomfortIndexAppTests.swift
@@ -11,19 +11,23 @@ import XCTest
 
 class WeatherModelTests: XCTestCase {
 
-    func testTemperatureString() {
-        let weatherModel1 = WeatherModel(conditionId: 1000, cityName: "sapporo", currentTemperature: 12.3, humidity: 10)
-        let weatherModel2 = WeatherModel(conditionId: 1003, cityName: "sapporo", currentTemperature: 12.6, humidity: 10)
+    let weatherModel1 = WeatherModel(conditionId: 1000, cityName: "sapporo", currentTemperature: 12.3, humidity: 10)
+    let weatherModel2 = WeatherModel(conditionId: 1003, cityName: "sapporo", currentTemperature: 12.6, humidity: 10)
 
+    func testTemperatureString() {
         XCTContext.runActivity(named: "temperatureStringのテスト") { _ in
             XCTAssertEqual(weatherModel1.temperatureString, "12", "気温が小数点第一位で四捨五入され、String型になっていること")
             XCTAssertEqual(weatherModel2.temperatureString, "13", "気温が小数点第一位で四捨五入され、String型になっていること")
         }
+    }
 
-        XCTContext.runActivity(named: "temperatureStringのテスト") { _ in
+    func testHumidityString() {
+        XCTContext.runActivity(named: "humidityStringのテスト") { _ in
             XCTAssertEqual(weatherModel1.humidityString, "10")
         }
+    }
 
+    func testConditionName() {
         XCTContext.runActivity(named: "conditionNameのテスト") { _ in
             XCTAssertEqual(weatherModel1.conditionName, "sun.max")
             XCTAssertEqual(weatherModel2.conditionName, "cloud.sun")
