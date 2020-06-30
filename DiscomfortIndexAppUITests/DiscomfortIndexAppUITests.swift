4fe4ceb1216303da2a2451aaf2738a5281510d05
ta <amida.tatsuya@gmail.com>
2020-06-30 23:41:10 +0900

    feat-予報ページのUIテストを追加しました

--- DiscomfortIndexAppUITests/DiscomfortIndexAppUITests.swift
+++ DiscomfortIndexAppUITests/DiscomfortIndexAppUITests.swift
@@ -34,4 +34,26 @@ class DiscomfortIndexAppUITests: XCTestCase {
         XCTAssertEqual(cityLabel.label, "Sapporo")
     }
 
+    func testForecast() {
+        let app = XCUIApplication()
+        let searchTextField = app.textFields["forecast_textfield"]
+        let searchButton = app.buttons["forecast_search_button"]
+        let cityLabel = app.staticTexts["forecast_city_label"]
+
+        // スワイプして予報ページに移動
+        app.otherElements["weather_view"].swipeLeft()
+
+        XCTAssert(searchTextField.exists)
+        XCTAssert(searchButton.exists)
+        XCTAssert(cityLabel.exists)
+
+        searchTextField.tap()
+        searchTextField.typeText("sapporo")
+        searchButton.tap()
+
+        // 都市名ラベルが更新されるまでの待機時間として２秒を設定
+        sleep(2)
+        XCTAssertEqual(cityLabel.label, "Sapporo")
+    }
+
 }
