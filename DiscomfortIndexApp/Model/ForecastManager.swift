10b648b1d3cb8323efeaa0c7011edc35fddf2f4c
ta <amida.tatsuya@gmail.com>
2020-07-07 16:34:37 +0900

    feat-予報ページの不快指数情報が更新されるようにしました（9amのみ）

--- DiscomfortIndexApp/Model/ForecastManager.swift
+++ DiscomfortIndexApp/Model/ForecastManager.swift
@@ -12,7 +12,7 @@ import Alamofire
 import SwiftyJSON
 
 protocol ForecastManagerDelegate: class {
-    func didUpdateForecast(forecastManager: ForecastManager, forecast: ForecastModel)
+    func didUpdateForecast(forecastManager: ForecastManager, forecast: [ForecastModel], discomfortIndex: [DiscomfortIndexModel])
     func didFailWithError(error: Error)
 }
 
@@ -40,7 +40,7 @@ class ForecastManager {
             case .success:
                 if let safeData = response.data {
                     if let forecast = self.parseJSONforForecast(forecastData: safeData) {
-                        self.delegate?.didUpdateForecast(forecastManager: self, forecast: forecast)
+                        self.delegate?.didUpdateForecast(forecastManager: self, forecast: forecast.0, discomfortIndex: forecast.1)
                     }
                 }
             case .failure(let error):
@@ -49,27 +49,34 @@ class ForecastManager {
         }
     }
 
-    func parseJSONforForecast(forecastData: Data) -> ForecastModel? {
+    func parseJSONforForecast(forecastData: Data) -> ([ForecastModel], [DiscomfortIndexModel])? {
         do {
             let json = try JSON(data: forecastData)
             let name = json["location"]["name"].stringValue
             let date = json["forecast"]["forecastday"][1]["date"].stringValue
             let sixAMWeatherId = json["forecast"]["forecastday"][1]["hour"][6]["condition"]["code"].intValue
             let sixAMTemperature = json["forecast"]["forecastday"][1]["hour"][6]["temp_c"].doubleValue
+            let sixAMHumidity = json["forecast"]["forecastday"][1]["hour"][6]["humidity"].intValue
             let nineAMWeatherId = json["forecast"]["forecastday"][1]["hour"][9]["condition"]["code"].intValue
             let nineAMTemperature = json["forecast"]["forecastday"][1]["hour"][9]["temp_c"].doubleValue
+            let nineAMHumidity = json["forecast"]["forecastday"][1]["hour"][9]["humidity"].intValue
             let twelvePMWeatherId = json["forecast"]["forecastday"][1]["hour"][12]["condition"]["code"].intValue
             let twelvePMTemperature = json["forecast"]["forecastday"][1]["hour"][12]["temp_c"].doubleValue
+            let twelvePMHumidity = json["forecast"]["forecastday"][1]["hour"][12]["humidity"].intValue
             let threePMWeatherId = json["forecast"]["forecastday"][1]["hour"][15]["condition"]["code"].intValue
             let threePMTemperature = json["forecast"]["forecastday"][1]["hour"][15]["temp_c"].doubleValue
+            let threePMHumidity = json["forecast"]["forecastday"][1]["hour"][15]["humidity"].intValue
             let sixPMWeatherId = json["forecast"]["forecastday"][1]["hour"][18]["condition"]["code"].intValue
             let sixPMTemperature = json["forecast"]["forecastday"][1]["hour"][18]["temp_c"].doubleValue
+            let sixPMHumidity = json["forecast"]["forecastday"][1]["hour"][18]["humidity"].intValue
             let ninePMWeatherId = json["forecast"]["forecastday"][1]["hour"][21]["condition"]["code"].intValue
             let ninePMTemperature = json["forecast"]["forecastday"][1]["hour"][21]["temp_c"].doubleValue
+            let ninePMHumidity = json["forecast"]["forecastday"][1]["hour"][21]["humidity"].intValue
 
-            let forecast = ForecastModel(cityName: name, date: date, sixAMConditionId: sixAMWeatherId, nineAMConditionId: nineAMWeatherId, twelvePMConditionId: twelvePMWeatherId, threePMConditionId: threePMWeatherId, sixPMConditionId: sixPMWeatherId, ninePMConditionId: ninePMWeatherId, sixAMTemperature: sixAMTemperature, nineAMTemperature: nineAMTemperature, twelvePMTemperature: twelvePMTemperature, threePMTemperature: threePMTemperature, sixPMTemperature: sixPMTemperature, ninePMTemperature: ninePMTemperature)
+            let forecastList = [ForecastModel(cityName: name, date: date, conditionId: sixAMWeatherId, temperature: sixAMTemperature, humidity: sixAMHumidity)]
+            let discomfortIndexList = [DiscomfortIndexModel(temperature: sixAMTemperature, humidity: sixAMHumidity)]
 
-            return forecast
+            return (forecastList, discomfortIndexList)
         } catch {
             delegate?.didFailWithError(error: error)
             return nil
